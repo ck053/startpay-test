70218139f572cc4c586fab216419983c80b26314
ck053 <kwok.charles53@gmail.com>
2025-07-15 17:37:22 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -255,7 +255,7 @@ export default function Home() {
           onClose={handleCloseModal}
         />
       )}
-      
+      <h1>Hi {username}!</h1>
       <h1 className="text-2xl font-bold mb-6 text-center">Digital Store 1.05</h1>
       
       <ItemsList 
