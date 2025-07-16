e1cb2d8dabe970d1c5aec0047a0c34f0330afb5d
ck053 <kwok.charles53@gmail.com>
2025-07-16 11:35:00 +0800

    j

--- app/game/SetBoard.js
+++ app/game/SetBoard.js
@@ -1,5 +1,7 @@
 export const SetBoard = () => {
+    return(
     <div className="gameboard">
-        <div id="tile1" style="background-image: url('@/public/Regular/0m.png');" className="tile"></div>
+        <div id="tile1" style="background-image: url('@/public/Regular/0m.png')" className="tile"></div>
     </div>
+    )
 }
