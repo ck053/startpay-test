58b9d7477cd96986ca28c6bcdb93facc00d00ad4
ck053 <kwok.charles53@gmail.com>
2025-07-16 14:09:00 +0800

    update

--- app/game/components/Board.tsx
+++ app/game/components/Board.tsx
@@ -3,7 +3,23 @@
 export default function Board() {
     return (
         <div className="gameboard app-container">
-        <div id="tile1" style={{ backgroundImage: "url('Regular/0m.png')" }}  className="tile"></div>
+        <div className="playerhand">
+                <div 
+                    id="tile1" 
+                    style={{ backgroundImage: "url('Regular/0m.png')" }} 
+                    className="tile"
+                ></div>
+                <div 
+                    id="tile2" 
+                    style={{ backgroundImage: "url('Regular/1m.png')" }} 
+                    className="tile"
+                ></div>
+                <div 
+                    id="tile3" 
+                    style={{ backgroundImage: "url('Regular/2m.png')" }} 
+                    className="tile"
+                ></div>
+            </div>
         </div>
     )
 }
